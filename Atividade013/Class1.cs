5e2f1ccf38d46a7b47e6a75637876d3c8160c4fa
caiovallee <caiohgvalle.14@gmail.com>
2023-04-10 14:16:46 -0300

    Adicionar arquivos de projeto.

---
+++ Atividade013/Class1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atividade013
+{
+    internal class Contacorrente
+    {
+        internal double saldo;
+        internal int numero;
+        internal double limite;
+        internal bool especial;
+        internal ArrayList movimentacoes;
+
+
+        internal Contacorrente (int numero, double saldo, bool especial,double limite)
+        {
+            this.numero = numero;
+            this.saldo = saldo;
+            this.especial = especial;
+            this.limite = limite;
+            this.movimentacoes = new ArrayList ();
+        }
+
+        public void Depositar(double valor)
+            {
+                string mensagem = ($"Realizado deposito de {valor}$");
+                this.movimentacoes.Add(mensagem);
+                this.saldo += valor;
+                Console.WriteLine($"O deposito de {valor}$  para a conta {this.numero} foi um sucesso");
+            }
+
+        public void Sacar(double valor)
+        {
+            double limiteSaque = this.saldo + this.limite;
+
+            if (valor <= limiteSaque)
+            {
+                string mensagem = ($"Realizado Saque de {valor}$");
+                this.saldo -= valor;
+                this.movimentacoes.Add(mensagem);
+                Console.WriteLine($"O saque de {valor}$  da conta  {this.numero} foi um sucesso");
+            }
+            else
+            {
+                Console.WriteLine("Não é possível realizar o saque. O valor informado excede o limite de saque.");
+            }
+        }
+
+        public void VisualizarSaldo()
+        {
+            Console.WriteLine($"O saldo na conta {this.numero} é de : {this.saldo} $");
+        }
+
+        public void VisualizarEstrato()
+        {
+            for (int i = 0; i < this.movimentacoes.Count; i++)
+            {
+                Console.WriteLine(this.movimentacoes[i]);
+            }
+        }
+
+        public void Transferir(double valor, Contacorrente objinicio, Contacorrente objDestino)
+        {
+            double limitetransferencia = this.saldo + this.limite;
+
+            if (valor <= limitetransferencia)
+            {
+                string mensagem = ($"Realizado transferencia  da conta {objinicio.numero} no valor de {valor}$ para conta {objDestino.numero}");
+                this.movimentacoes.Add(mensagem);
+                objinicio.saldo -= valor;
+                objDestino.saldo += valor;
+                Console.WriteLine(mensagem);
+            }
+            else
+            {
+                Console.WriteLine("Não é possível realizar a transferencia. O valor informado excede o limite de transferencia.");
+            }
+            
+            
+            
+        }
+
+
+    }
+}
---
+++ Atividade013/Program.cs
@@ -0,0 +1,46 @@
+﻿using System.ComponentModel;
+using System.Drawing;
+using System.Runtime.Intrinsics.X86;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Atividade013
+{
+    internal class Program
+    {
+//        Uma conta corrente possui um número, um saldo, um status que informa se ela é especial ou não, um limite e um conjunto
+//        de movimentações.Uma movimentação possui um valor e uma informação se ela é uma movimentação de crédito ou débito. 
+
+//Cada conta terá operações de saques, depósitos, visualização de saldo, visualização de extrato e transferência entre contas.
+//        Uma conta corrente só pode fazer saques desde que o valor não exceda o limite de saque que é o limite + saldo.
+
+//Não precisa implementar a interação com usuário.
+
+        static void Main(string[] args)
+        {
+
+            Contacorrente conta1 = new Contacorrente(1, 600, true, 200);
+            Contacorrente conta2 = new Contacorrente(2, 600, true, 200);
+
+            conta1.Sacar(100);
+            Console.WriteLine();
+            conta1.Depositar(200);
+            Console.WriteLine();
+            conta1.Transferir(200, conta2, conta1);
+            Console.WriteLine();
+            conta1.VisualizarEstrato();
+            Console.WriteLine();
+            conta1.VisualizarSaldo();
+            
+            Console.ReadLine();
+
+
+
+            
+            
+
+
+
+
+        }
+    }
+}
